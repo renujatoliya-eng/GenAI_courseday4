7d489a1ad4a8871f1a7a8d64dfd430129bb99d0d
renujatoliya-eng <renu.jatoliya@heromotocorp.com>
2026-06-26 15:05:21 +0530

    Update GenAI.py

--- GenAI.py
+++ GenAI.py
@@ -20,21 +20,7 @@ st.markdown(
 robo = genai.Client( api_key=st.secrets["GOOGLE_API_KEY"])
 mychat = robo.chats.create(model="gemini-flash-lite-latest")
 
-#Placeholder for the response
-response_placeholder = st.empty()
-
-question = st.text_input("", placeholder="Enter your Python question here...")
-
-col1, col2, col3 = st.columns([4, 1, 4])
-
-with col2:
-  send =st.button("Send")
-if send:
-  response = mychat.send_message(question)
-  response_placeholder.write(response.text)
-
-mychat = robo.chats.create(model="gemini-flash-lite-latest")
-#Placeholder for the response
+Placeholder for the response
 response_placeholder = st.empty()
 
 question = st.text_input("", placeholder="Enter your Python question here...")
